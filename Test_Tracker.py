f32fdbd5bcb215c23353c8df056b433b4f07caa6
Cec0088 <gundam99@hotmail.co.uk>
2023-12-02 12:20:48 +0800

    2nd commit
    added python notebook

--- Test_Tracker.py
+++ Test_Tracker.py
@@ -3,14 +3,14 @@
 
 # # Test Tracker
 
-# In[9]:
+# In[1]:
 
 
 #importing modules/packages 
 import csv
 
 
-# In[10]:
+# In[2]:
 
 
 #saves current user input to corresponding csv file.
@@ -21,7 +21,7 @@ def write_to_file(file_name, data):
         writer.writerow([data])
 
 
-# In[28]:
+# In[3]:
 
 
 #formating the csv file beacuse its not inputing a intger as input :(
@@ -38,22 +38,23 @@ def format_data(file_name):
     return container
 
 
-# In[41]:
+# In[9]:
 
 
 # looks at highest, lowest, current and last result and compares them
 def anlayse_data(file_name):
     results = format_data(file_name)
-    print("This is all your results to date: "results)
+    print("This is all your results to date: ")
+    print(results)
 
     current = results[-1]   
-    print("This is your score now: "current)
+    print("This is your score now: ", current)
     last = results[-2]
-    print("This is your previous score: "last)
+    print("This is your previous score: ", last)
     highest = max(results)
-    print("This is your highest score: ")
+    print("This is your highest score: ", highest)
     lowest = min(results)
-    print("This is your lowest score: "lowest)
+    print("This is your lowest score: ", lowest)
     
     print("Let's guage your performance?")
     if current > last:
@@ -64,7 +65,7 @@ def anlayse_data(file_name):
         print("your score stayed the same, are revising enough")
 
 
-# In[31]:
+# In[10]:
 
 
 #makes sure they enter an integer
@@ -72,10 +73,11 @@ def reminders():
     print("Remember entry is a integer out of 100!")
 
 
-# In[ ]:
+# In[11]:
 
 
 #this is where the magic happens
+
 print("1. Maths")
 print("2. Science")
 print("3. English")
@@ -90,7 +92,6 @@ if subject == "1":
         format_data("MathResults.csv")
         anlayse_data("MathResults.csv")
          #print(Maths)
-
 elif subject == "2":
         reminders()
         Science = input("please enter your test result")
@@ -99,7 +100,6 @@ elif subject == "2":
         format_data("ScienceResults.csv")
         anlayse_data("ScienceResults.csv")
         #print(Science)
-
 elif subject == "3":
         reminders()
         English = input("please enter your test result")
@@ -108,10 +108,15 @@ elif subject == "3":
         format_data("EnglishResults.csv")
         anlayse_data("EnglishResults.csv")
         #print(English)
-
 elif subject == "e" or Subject == "E":
         print("program ended")
         quit()
 #print(result)
 #print(type(result))
+
+
+# In[ ]:
+
+
+
 
